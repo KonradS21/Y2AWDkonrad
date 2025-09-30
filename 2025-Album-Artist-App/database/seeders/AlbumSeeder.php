91fa15273066ffb88c88d9aeabece4cf6f8b5952
Konrad <N00243433@iadt.ie>
2025-09-30 15:48:05 +0100

    progress 30.09

--- 2025-Album-Artist-App/database/seeders/AlbumSeeder.php
+++ 2025-Album-Artist-App/database/seeders/AlbumSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Album;
 use Carbon\Carbon;
 
 class AlbumSeeder extends Seeder{
