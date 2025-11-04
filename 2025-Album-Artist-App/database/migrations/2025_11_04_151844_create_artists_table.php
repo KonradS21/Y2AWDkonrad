1e20e41ce8c179a6cd19192fe82f7f1659f2f4bc
Konrad <N00243433@iadt.ie>
2025-11-04 16:14:57 +0000

    started working on ca2, created models etc.

---
+++ 2025-Album-Artist-App/database/migrations/2025_11_04_151844_create_artists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('album_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('stage_name');
+            $table->string('birth_name');
+            $table->date('birth_date');
+            $table->string('biography', 2000);
+            $table->integer('debut_year');
+            $table->string('image');
+            $table->integer('no_of_grammys')->default(0);
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
